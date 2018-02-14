c2e60d4cd8cc7239b4ab7e2ea521dceab5ec6d3f
Sharon O'Malley <sharonomalley743@gmail.com>
2018-02-14 17:20:42 +0000

    tostring tune

--- ie/dit/Tune.java
+++ ie/dit/Tune.java
@@ -7,31 +7,37 @@ public class Tune
 	private String title;
 	private String altTitle;
 	private String notation;
-
-	public int getX()
-	{
-	}
-
-	public String getTitle()
-	{
-	}
 	
-	public String getAltTitle()
-	{
-	}
-
+	public Tune()
+    {
+     //   ();
+	//	();
+	//	();
+    }
+	
 	public String getNotation()
 	{
 	}
 	
 	public String toString()
     	{
-        	/*StringBuffer sb = new StringBuffer();
-        	for(String word:words)
+        	StringBuffer sb = new StringBuffer();
+        	for(String word:x)
         	{
             		sb.append(word + ", ");
-        	}
+					for(String wordd:title)
+						{
+							sb.append(word + ", ");
+						}
+					for(String worddd:altTitle)
+						{
+							if(worddd != null)
+							{
+								sb.append(word + ", ");
+							}
+						}
+			}
 
  	       	return sb.toString();
-	*/}
+	}
 }
